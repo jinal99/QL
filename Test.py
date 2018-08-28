ba166c9f91b801d10f85cb25e5eff3daf6240476
jinal99 <thakkarjinal99@gmail.com>
2018-08-28 17:48:12 +0530

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,33 +1,46 @@
-# -*- coding: utf-8 -*-
+import http.client, urllib.parse, json, pandas as pd, requests
 
-import http.client, urllib.parse, json, time, pandas as pd, requests
-
-# NOTE: Replace this with a valid host name.
 host = "my-qna.azurewebsites.net"
-
-# NOTE: Replace this with a valid endpoint key.
-# This is not your subscription key.
-# To get your endpoint keys, call the GET /endpointkeys method.
 endpoint_key = "af78bfae-6430-4975-ae85-c46c2ac614a2"
-
-# NOTE: Replace this with a valid knowledge base ID.
-# Make sure you have published the knowledge base with the
-# POST /knowledgebases/{knowledge base ID} method.
 kb = "aac4c7b2-0138-4395-bf63-8ba96112dcbc"
-
 method = "/qnamaker/knowledgebases/aac4c7b2-0138-4395-bf63-8ba96112dcbc/generateAnswer"
-
+#question1: 30 minute delivery
+#question: otp undelivered
 question = {
-    'question': 'validity gift card ',
+    'question': 'otp undelivered',
     'top': 1
 }
 
+def display_answer(result):
+    load_result = json.loads(result)
+    values_result = load_result.values()
+    for t in values_result:
+        return t[0]['answer']
+    
+def get_score(result):
+    load_result = json.loads(result)
+    values_result = load_result.values()
+    for t in values_result:
+        score = t[0]['score']
+        return score
+
+def get_predicted_question(result):
+    load_result = json.loads(result)
+    values_result = load_result.values()
+    for t in values_result:
+        temp = t[0]['questions']
+        for l in temp:
+            return l
+
+def get_intent(result):
+    temp1 = result['topScoringIntent']
+    return temp1['intent']       
+    
 def pretty_print (content):
-# Note: We convert content to and from an object so we can pretty-print it.
     return json.dumps(json.loads(content), indent=4)
 
 def get_answers (path, content):
-    print ('Calling ' + host + path )
+    
     headers = {
         'Authorization': 'EndpointKey ' + endpoint_key,
         'Content-Type': 'application/json',
@@ -41,46 +54,47 @@ def get_answers (path, content):
 # Convert the request to a string.
 content = json.dumps(question)
 result = get_answers (method, content)
-temp = json.loads(result)
-temp1 = temp.values()
-score = 0
-for t in temp1:
-    score = t[0]['score']
-print(pretty_print(result))
-
-headers = {
-    # Request headers
-    'Ocp-Apim-Subscription-Key': 'e6a8d45d-f346-413e-9aa3-98a08eff6376',
-}
-
-params ={
-    # Query parameter
-    'q': question['question']
-}
-
-try:
-    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/bac225d4-73f5-4485-993d-08a9db2b7b63?subscription-key=391d7db47c6d40a2ba262e8b90420db4&timezoneOffset=0', params=params)
-    print(r.json())
-
-except Exception as e:  
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-####################################
+print('\n')
+json.loads(result)
+print("Predicted Question:"+get_predicted_question(result))
+print("Actual question: "+question['question'])
+print('\n')
+print("Answer: "+display_answer(result))
+print('\n')
+score  = get_score(result)
+print("Confidence score: "+str(score))
+print('\n')
+if score < 50:
+    print('Since confidence score less than 50, calling LUIS.....')
+    headers = {
+        'Ocp-Apim-Subscription-Key': 'e6a8d45d-f346-413e-9aa3-98a08eff6376',
+    }
     
-# importing the requests library
+    params ={
+        'q': question['question']
+    }
+    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/bac225d4-73f5-4485-993d-08a9db2b7b63?subscription-key=391d7db47c6d40a2ba262e8b90420db4&timezoneOffset=0', params=params)
+    print("New Question: "+get_intent(r.json()))
+    new_intent = get_intent(r.json())
+    
+    question = {
+        'question': new_intent,
+        'top': 1
+    }
+    
+    content = json.dumps(question)
+    result = get_answers (method, content)
+    print('\n')
+    json.loads(result)
+    print("Predicted Question:"+get_predicted_question(result))
+    print("Actual question: "+question['question'])
+    print('\n')
+    print("Answer: "+display_answer(result))
+    print('\n')
+    print("Confidence score: "+str(get_score(result)))
 
 
+   
+    
 
-header= {
-       'Host': 'westus.api.cognitive.microsoft.com',
-'Content-Type': 'application/json',
-'Ocp-Apim-Subscription-Key': '391d7db47c6d40a2ba262e8b90420db4'
-        }
-# api-endpoint
-
-# sending get request and saving the response as response object
-r = requests.post(r"https://westus.api.cognitive.microsoft.com/luis/api/v2.0/apps/bac225d4-73f5-4485-993d-08a9db2b7b63/versions/0.1/intents",json.dumps(header),json.dumps({"Name": 'hello'}))
-r.status_code
-# extracting data in json format
-data = r.json()
 
